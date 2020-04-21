7070bcdde31ff308f2ba37411841aacdc3043097
groot8 <nader@getspokn.com>
2020-04-21 02:21:16 +0200

    :package: adding eslint

--- app.js
+++ app.js
@@ -1,29 +1,32 @@
-var express = require("express");
-var chalk = require("chalk");
-var debug = require("debug")("app");
-var morgan = require("morgan");
-var path = require("path");
-var app = express();
+const express = require('express');
+const chalk = require('chalk');
+// eslint-disable-next-line import/no-unresolved
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
 
-app.use(morgan("tiny"));
-app.use(express.static(path.join(__dirname, "/public/")));
+const app = express();
+
+app.use(morgan('tiny'));
+
+app.use(express.static(path.join(__dirname, '/public/')));
 app.use(
-  "/css",
-  express.static(path.join(__dirname, "/node_modules/bootstrap/dist/css"))
+  '/css',
+  express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')),
 );
 app.use(
-  "/js",
-  express.static(path.join(__dirname, "/node_modules/bootstrap/dist/js"))
+  '/js',
+  express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')),
 );
 app.use(
-  "/js",
-  express.static(path.join(__dirname, "/node_modules/jquery/dist"))
+  '/js',
+  express.static(path.join(__dirname, '/node_modules/jquery/dist')),
 );
 
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "views", "index.html"));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-app.listen(4242, function () {
-  debug(`listening on port ${chalk.green("4242")}`);
+app.listen(4242, () => {
+  debug(`listening on port ${chalk.green('4242')}`);
 });
