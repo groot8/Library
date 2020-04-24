7131cf849384eb2b31435f76ae671fc646908382
groot8 <nader@getspokn.com>
2020-04-24 19:24:26 +0200

    seperate files

---
+++ src/routes/bookRoutes.js
@@ -0,0 +1,42 @@
+/* eslint-disable indent */
+const express = require('express');
+
+const bookRouter = express.Router();
+
+const books = [
+    {
+        title: 'book1',
+        genre: 'art',
+        author: 'author1',
+        read: false
+    },
+    {
+        title: 'book2',
+        genre: 'art',
+        author: 'author2',
+        read: false
+    },
+    {
+        title: 'book3',
+        genre: 'art',
+        author: 'author3',
+        read: false
+    },
+    {
+        title: 'book4',
+        genre: 'art',
+        author: 'author4',
+        read: false
+    }
+];
+
+bookRouter.route('/')
+    .get((req, res) => {
+        res.render('books', {
+            nav: [{ link: '/books', title: 'Books' }, { link: '/authors', title: 'Authors' }],
+            title: 'Library',
+            books
+        });
+    });
+
+module.exports = bookRouter;
